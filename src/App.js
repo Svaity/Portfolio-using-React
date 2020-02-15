5a2846607545c5ca596eae51e8e2f674ca18222f
Shrey <svaity@stevens.edu>
2020-02-15 15:27:09 -0500

    First Commit
    Created React Protfolio

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
+import profile from "./assets/profile1.jpg";
+
+class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
+
+  render() {
+    return (
+      <div>
+        <img src={profile} alt="profile" className="profile" />
+        <h1>Shrey Vaity</h1>
+        <p>My name is Shrey, Im a Software Engineer</p>
+        <p> Im looking forward to working on cool project</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in New York and code everyday</p>
+            <p>
+              My favorite language is Javscript, and i think React.js is awesome
+            </p>
+            <p>Besides coding, I also love Music and Ramen</p>
+            <button onClick={this.toggleDisplayBio}>Show Less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )}
+        <hr />
+        <Projects />
+        <hr />
+        <SocialProfiles />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,41 @@
+import project1 from "../assets/Project11.gif";
+import project2 from "../assets/Project22.jpg";
+import project3 from "../assets/Project33.jpg";
+import project4 from "../assets/project4.jpg";
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: "Self Driving Car Software",
+    description:
+      "This powerful simulation built using hands on skills in neural networks which could be brought to any project.",
+    link: "https://github.com/Svaity/Self-Driving-Car-Software",
+    image: project1
+  },
+  {
+    id: 2,
+    title: "ReactMusic",
+    description:
+      "A full-stack app from scratch with a GraphQL API made with Python (Django and Graphene), a React client app",
+    link: "https://github.com/Svaity/ReactMusic",
+    image: project2
+  },
+  {
+    id: 3,
+    title: "Pet adoption System",
+    description:
+      "Pet adoption system for shelters that makes it easy to track animal status, classify and manage animal data.",
+    link: "https://github.com/Svaity/Pet-adoption-System",
+    image: project3
+  },
+  {
+    id: 4,
+    title: "Portfolio using Django Postgres",
+    description:
+      "Used Python with Django with Python , Bootstrap, Postgres to create a portfolio website",
+    link: "https://github.com/Svaity/Portfolio-using-Django-Postgres",
+    image: project4
+  }
+];
+
+export default PROJECTS;
